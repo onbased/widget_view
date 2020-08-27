6d431ea305fd739b5289d64def5b7ad81b5a99fd
Naoto Miyamoto <naoto@polyflection.com>
2020-08-28 06:59:18 +0900

    Add protected annotations.

--- lib/src/widget_view.dart
+++ lib/src/widget_view.dart
@@ -2,13 +2,16 @@ import 'package:flutter/widgets.dart';
 
 abstract class StatelessWidgetView<W extends StatelessWidget>
     extends StatelessWidget {
+  @protected
   final W controller;
   const StatelessWidgetView(this.controller, {Key key}) : super(key: key);
 }
 
 abstract class StatefulWidgetView<W extends StatefulWidget, S extends State>
     extends StatelessWidget {
+  @protected
   final S controller;
+  @protected
   W get widget => controller.widget;
   const StatefulWidgetView(this.controller, {Key key}) : super(key: key);
 }
