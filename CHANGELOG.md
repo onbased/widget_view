4184aa8208ed46829ef6ce9216c0850af7c48eaf
Naoto Miyamoto <naoto@polyflection.com>
2020-08-31 15:36:33 +0900

    Document updates.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.2
+
+- Update Documents.
+
 ## 0.1.1
 
 - Add protected annotations.
--- lib/src/widget_view.dart
+++ lib/src/widget_view.dart
@@ -1,12 +1,65 @@
 import 'package:flutter/widgets.dart';
 
-abstract class StatelessWidgetView<W extends StatelessWidget>
-    extends StatelessWidget {
-  @protected
-  final W controller;
-  const StatelessWidgetView(this.controller, {Key key}) : super(key: key);
-}
-
+/// A widget view of a [State] as a stateful controller in a [StatefulWidget].
+///
+/// # Example
+///
+/// ```dart
+/// class MyHomePage extends StatefulWidget {
+///   MyHomePage({Key key, this.title}) : super(key: key);
+///   final String title;
+///
+///   @override
+///   _MyHomePageController createState() => _MyHomePageController();
+/// }
+///
+/// class _MyHomePageController extends State<MyHomePage> {
+///   int counter = 0;
+///
+///   void incrementCounter() {
+///     setState(() {
+///       counter++;
+///     });
+///   }
+///
+///   @override
+///   Widget build(BuildContext context) => _MyHomePageView(this);
+/// }
+///
+/// class _MyHomePageView
+///     extends StatefulWidgetView<MyHomePage, _MyHomePageController> {
+///   const _MyHomePageView(_MyHomePageController controller, {Key key})
+///       : super(controller, key: key);
+///
+///   @override
+///   Widget build(BuildContext context) {
+///     return Scaffold(
+///       appBar: AppBar(
+///         title: Text(widget.title),
+///       ),
+///       body: Center(
+///         child: Column(
+///           mainAxisAlignment: MainAxisAlignment.center,
+///           children: <Widget>[
+///             Text(
+///               'You have pushed the button this many times:',
+///             ),
+///             Text(
+///               '${controller.counter}',
+///               style: Theme.of(context).textTheme.headline4,
+///             ),
+///           ],
+///         ),
+///       ),
+///       floatingActionButton: FloatingActionButton(
+///         onPressed: controller.incrementCounter,
+///         tooltip: 'Increment',
+///         child: Icon(Icons.add),
+///       ),
+///     );
+///   }
+/// }
+/// ```
 abstract class StatefulWidgetView<W extends StatefulWidget, S extends State>
     extends StatelessWidget {
   @protected
@@ -14,4 +67,12 @@ abstract class StatefulWidgetView<W extends StatefulWidget, S extends State>
   @protected
   W get widget => controller.widget;
   const StatefulWidgetView(this.controller, {Key key}) : super(key: key);
+}
+
+/// A widget view of a [StatelessWidget] as a stateless controller.
+abstract class StatelessWidgetView<W extends StatelessWidget>
+    extends StatelessWidget {
+  @protected
+  final W controller;
+  const StatelessWidgetView(this.controller, {Key key}) : super(key: key);
 }
